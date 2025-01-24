c93a80809f1ecebcd6ca4a9d210effa6c40af9b0
Trane20211 <hermes.demicheli@hotmail.com>
2025-01-24 15:43:06 +0100

    Step 3

--- webapp/controller/Startpage.controller.js
+++ webapp/controller/Startpage.controller.js
@@ -1,37 +1,57 @@
-sap.ui.define(["sap/ui/core/mvc/Controller",
-	"sap/ui/core/format/NumberFormat",
-	"sap/base/strings/formatMessage"
-], function (Controller, NumberFormat, formatMessage) {
-  "use strict";
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/format/NumberFormat",
+    "sap/base/strings/formatMessage",
+    "jquery.sap.global",
+  ],
+  function (Controller, JSONModel, NumberFormat, formatMessage, jQuery) {
+    "use strict";
 
-  return Controller.extend(
-    "sap.suite.ui.commons.demokit.tutorial.icecream.01.controller.Startpage",
-    {
-      onInit: function () {},
+    return Controller.extend(
+      "sap.suite.ui.commons.demokit.tutorial.icecream.03.controller.Startpage",
+      {
+        onInit: function () {
+          var sDataPath =
+            //sap.ui.require.toUrl
+            jQuery.sap.getResourcePath("sap/suite/ui/commons/demokit/tutorial/icecream/01/model/data") +
+            "/News.json";
+          var oModel = new JSONModel(sDataPath);
+          this.getView().setModel(oModel, "news");
+        },
 
-      getProgress: function (aNodes) {
-        if (!aNodes || aNodes.length === 0) {
-          return 0;
-        }
-        var iSum = 0;
-        for (var i = 0; i < aNodes.length; i++) {
-          iSum += aNodes[i].state === "Positive";
-        }
-        var fPercent = (iSum / aNodes.length) * 100;
-        return fPercent.toFixed(0);
-      },
+        formatMessage: formatMessage,
 
-      getEntityCount: function (entities) {
-        return (entities && entities.length) || 0;
-		},
-	  
-		formatNumber: function (value) {
-			var oFloatFormatter = NumberFormat.getFloatInstance({
-				style: "short",
-				decimal: 1
-			});
-			return oFloatFormatter.format(value);
-	  },
-    }
-  );
-});
+        getProgress: function (aNodes) {
+          if (!aNodes || aNodes.length === 0) {
+            return 0;
+          }
+          var iSum = 0;
+          for (var i = 0; i < aNodes.length; i++) {
+            iSum += aNodes[i].state === "Positive";
+          }
+          var fPercent = (iSum / aNodes.length) * 100;
+          return fPercent.toFixed(0);
+        },
+
+        getEntityCount: function (entities) {
+          return (entities && entities.length) || 0;
+        },
+
+        formatNumber: function (value) {
+          var oFloatFormatter = NumberFormat.getFloatInstance({
+            style: "short",
+            decimals: 1,
+          });
+          return oFloatFormatter.format(value);
+        },
+
+        formatJSONDate: function (date) {
+          var oDate = new Date(Date.parse(date));
+          return oDate.toLocaleDateString();
+        },
+      }
+    );
+  }
+);
